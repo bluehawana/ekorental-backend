be24503bef96c8a96aa56ff0a52dec814f8816c6
bluehawana <bluehawana@gmail.com>
2025-01-29 13:49:54 +0100

    modified some files for guranting the project be running well

--- src/test/java/com/bluehawana/rentingcarsys/RentingcarsysApplicationTests.java
+++ src/test/java/com/bluehawana/rentingcarsys/RentingcarsysApplicationTests.java
@@ -2,12 +2,13 @@ package com.bluehawana.rentingcarsys;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RentingcarsysApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
